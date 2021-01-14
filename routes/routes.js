e907e43b8b27d61b139d2202527338821c856d6b
Hannes Hieronimi <git@hieronimi.xyz>
2021-01-14 20:46:55 +0100

    backend

--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,39 @@
+let color = {
+  r: 1,
+  g: 2,
+  b: 3,
+};
+
 const router = (app) => {
+  //implememt color picker her
   app.get("/", (request, response) => {
     response.send({
       message: "Node.js and Express REST API",
     });
   });
+
+  // respond with rgb value
   app.get("/color", (request, response) => {
     response.send(color);
   });
-};
 
-const color = {
-  r: 1,
-  g: 2,
-  b: 3,
+  // Update the rgb value
+  app.put("/color", (request, response) => {
+    let req = request.body;
+    console.log(req);
+
+    color.r = Number.isInteger(parseInt(req.r))
+      ? parseInt(req.r) % 256
+      : color.r;
+    color.g = Number.isInteger(parseInt(req.g))
+      ? parseInt(req.g) % 256
+      : color.g;
+    color.b = Number.isInteger(parseInt(req.b))
+      ? parseInt(req.b) % 256
+      : color.b;
+
+    response.send("Updated the color");
+  });
 };
 
 // Export the router
